fedd77c329b6cae73480c9ef5c38a7c11a0d4473
kads9905 <kadambari9905@gmail.com>
2026-06-19 01:03:37 +0530

    feat: create application layout and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+     <header className="flex justify-between items-center mb-8">
+
+      <h1 className="text-4xl font-bold">
+
+        <span className="text-white">
+          POMO
+        </span>
+
+        <span className="text-orange-500">
+          TASK
+        </span>
+
+      </h1>
+
+    </header>
+  )
+}
+
+export default Header
