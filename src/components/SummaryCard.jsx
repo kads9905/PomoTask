f2d191c4008a4f4969a8c1738584455386f57f3e
kads9905 <kadambari9905@gmail.com>
2026-06-21 11:10:49 +0530

    feat: show empty task state

--- src/components/SummaryCard.jsx
+++ src/components/SummaryCard.jsx
@@ -2,15 +2,31 @@ import React from 'react'
 
 const SummaryCard = ({ completedTasks, totalTasks }) => {
   return (
-    <div className="bg-zinc-900 border border-zinc-800 rounded-xl p-6">
+    <div className="bg-zinc-900 border border-zinc-800 rounded-3xl p-8 min-h-44 flex justify-between items-center">
 
-      <h2 className="text-white text-xl font-semibold">
-        {completedTasks} / {totalTasks} Tasks Done
-      </h2>
+      {/* Left Side */}
+      <div>
+        <h1 className="text-5xl font-bold text-white">
+          Todo Done
+        </h1>
 
-      <p className="text-zinc-400 mt-2">
-        Start adding tasks to stay productive.
-      </p>
+        <p className="text-zinc-400 text-xl mt-4">
+          Keep it up
+        </p>
+      </div>
+
+      {/* Right Side Circle */}
+      <div className="w-40 h-40 rounded-full bg-orange-500 flex flex-col items-center justify-center shrink-0">
+
+        <h2 className="text-4xl font-bold text-black">
+          {completedTasks}/{totalTasks}
+        </h2>
+
+        <p className="text-black text-lg">
+          Tasks Done
+        </p>
+
+      </div>
 
     </div>
   )
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import TaskItem from './TaskItem';
 
 const TaskList = ({ tasks, toggleTask, deleteTask }) => {
+  if (tasks.length === 0) {
+    return (
+      <div className="bg-zinc-900 border border-zinc-800 rounded-3xl p-10 text-center mt-8">
+
+        <h2 className="text-2xl font-bold text-white">
+          No tasks yet
+        </h2>
+
+        <p className="text-zinc-400 mt-4">
+          Add your first task above
+        </p>
+
+      </div>
+    );
+  }
   return (
     <div className="mt-6">
 
